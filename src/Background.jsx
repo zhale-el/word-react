7dd0c846f29bc4ac72cbe8c232899fc6bd3f3b64
zhaleh <zhalehelmi@gmail.com>
2024-10-20 13:58:13 +0330

    init

---
+++ src/Background.jsx
@@ -0,0 +1,5 @@
+const Background = () => {
+  return <div>background</div>;
+};
+
+export default Background;
---
+++ src/Container.jsx
@@ -0,0 +1,5 @@
+const Container = () => {
+  return <div>container</div>;
+};
+
+export default Container;
